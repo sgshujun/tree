484aa86f890568c63d238a19eb37f2b7f70536b9
Yuqian Ma <mayq0422@gmail.com>
2023-05-30 22:01:46 +0800

    basic idea POC

---
+++ sketch.js
@@ -0,0 +1,152 @@
+
+const serial = new p5.WebSerial();
+
+let portButton;
+
+function setup() {
+  // createCanvas(800,800); // make the canvas
+  
+  // check to see if serial is available:
+  if (!navigator.serial) {
+    alert("WebSerial is not supported in this browser. Try Chrome or MS Edge.");
+  }
+  // if serial is available, add connect/disconnect listeners:
+  navigator.serial.addEventListener("connect", portConnect);
+  navigator.serial.addEventListener("disconnect", portDisconnect);
+  // check for any ports that are available:
+  serial.getPorts();
+  // if there's no port chosen, choose one:
+  serial.on("noport", makePortButton);
+  // open whatever port is available:
+  serial.on("portavailable", openPort);
+  // handle serial errors:
+  serial.on("requesterror", portError);
+  // handle any incoming serial data:
+  serial.on("data", serialEvent);
+  serial.on("close", makePortButton);
+  
+}
+
+// function draw() {
+
+// }
+
+
+const container = document.querySelector('#container');
+const mainVideos = document.querySelectorAll('.main-video');
+const info = document.querySelector('#info');
+
+// TODO: make this work for multiple videos
+const video = mainVideos[0];
+
+
+
+document.addEventListener('keydown', (e) => {
+  console.log(e);
+	if (Number.isInteger(parseInt(e.key))) {
+		mainVideos[parseInt(e.key) - 1].play();
+		return;
+	}
+  switch (e.key) {
+    case 'f':
+      container.requestFullscreen();
+      break;
+    case 'p':
+      video.play();
+      break;
+    case 's':
+      video.pause();
+      break;
+    case ']':
+      video.playbackRate += 0.1;
+      info.textContent = video.playbackRate;
+      break;
+    case '[':
+      video.playbackRate -= 0.1;
+      info.textContent = video.playbackRate;
+      break;
+    case '.':
+      video.currentTime = (video.currentTime + 1) % video.duration;
+      break;
+    default:
+      break;
+  }
+});
+
+
+// if there's no port selected,
+// make a port select button appear:
+function makePortButton() {
+  // create and position a port chooser button:
+  portButton = createButton("choose port");
+  portButton.position(10, 10);
+  // give the port button a mousepressed handler:
+  portButton.mousePressed(choosePort);
+}
+
+// make the port selector window appear:
+function choosePort() {
+  serial.requestPort();
+}
+
+// open the selected port, and make the port
+// button invisible:
+function openPort() {
+  // wait for the serial.open promise to return,
+  // then call the initiateSerial function
+  serial.open().then(initiateSerial);
+
+  // once the port opens, let the user know:
+  function initiateSerial() {
+    console.log("port open");
+    serial.write("x");
+   
+  }
+  // hide the port button once a port is chosen:
+  if (portButton) portButton.hide();
+}
+
+function serialEvent() {
+  // read a string from the serial port
+  // until you get carriage return and newline:
+  let inString = serial.readStringUntil("\r\n");
+  //check to see that there's actually a string there:
+  
+  if (inString){ 
+     
+    
+    let sensors = split(inString , "," );
+    
+
+    // locH = map(int(sensors[0]), 0,1023, 0, width);
+    // locV = map(int(sensors[1]), 0, 1023,0, height);
+    serial.write("x");
+    
+    if (int(sensors[0]) > 500) {
+      video.play();
+    }
+    
+    
+  }
+}
+
+
+
+// pop up an alert if there's a port error:
+function portError(err) {
+  alert("Serial port error: " + err);
+}
+
+// try to connect if a new serial port
+// gets added (i.e. plugged in via USB):
+function portConnect() {
+  console.log("port connected");
+  serial.getPorts();
+}
+
+// if a port is disconnected:
+function portDisconnect() {
+  serial.close();
+  console.log("port disconnected");
+}
+
